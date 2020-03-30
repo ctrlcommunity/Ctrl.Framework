665b7df46ce85aae8d8df84dcdddc2038df831b3
HueiFeng <695979933@qq.com>
2020-03-30 12:15:26 +0800

    Added Log files

---
+++ src/Ctrl.Application/CtrlCloud/Logs/SystemExceptionLogLogic.cs
@@ -0,0 +1,47 @@
+﻿using Ctrl.Domain.DataAccess.Log;
+using Ctrl.Domain.Models.Dtos.Logs;
+using Ctrl.Domain.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+
+namespace Ctrl.Domain.Business.Log
+{
+    public class SystemExceptionLogLogic : CrudAppService<SystemExceptionLog, SystemExceptionLogDto,Guid>, ISystemExceptionLogLogic, IScopedDependency
+    {
+        private readonly ISystemExceptionLogRepository _exceptionLogRepository;
+        public SystemExceptionLogLogic(IRepository<SystemExceptionLog, Guid> repository,
+            ISystemExceptionLogRepository exceptionLogRepository) : base(repository)
+        {
+            this._exceptionLogRepository = exceptionLogRepository;
+        }
+        #region 构造函数
+     
+        //public SystemExceptionLogLogic(ISystemExceptionLogRepository exceptionLogRepository)
+        //    : base(exceptionLogRepository)
+        //{
+        //    this._exceptionLogRepository = exceptionLogRepository;
+        //}
+
+        /// <summary>
+        ///     异常信息分页
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        public async Task<PagedResultDto<SystemExceptionLogDto>> PagingExceptionLogQuery(SystemExceptionLogResultRequestDto query)
+        {
+            var list = await _exceptionLogRepository.GetListAsync(query);
+            var totalCount = await _exceptionLogRepository.GetCountAsync(query);
+
+            return new PagedResultDto<SystemExceptionLogDto>(
+                totalCount,
+                ObjectMapper.Map<List<SystemExceptionLog>, List<SystemExceptionLogDto>>(list)
+                );
+        }
+        #endregion
+    }
+}
---
+++ src/Ctrl.Application/CtrlCloud/Logs/SystemOperationLogLogic.cs
@@ -0,0 +1,43 @@
+﻿using Ctrl.Domain.DataAccess.Log;
+using Ctrl.Domain.Models.Dtos.Logs;
+using Ctrl.Domain.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+
+namespace Ctrl.Domain.Business.Log
+{
+    public class SystemOperationLogLogic : CrudAppService<SystemOperateLog, SystemOperateLogOutput,
+        Guid, SystemOperateLogResultRequestDto>, ISystemOperationLogLogic, IScopedDependency
+    {
+        private readonly ISystemOperationLogRepository _systemOperationLogRepository;
+        public SystemOperationLogLogic(IRepository<SystemOperateLog, Guid> repository, ISystemOperationLogRepository systemOperationLogRepository) : base(repository)
+        {
+            this._systemOperationLogRepository = systemOperationLogRepository;
+        }
+        #region 构造函数
+
+        #endregion
+        /// <summary>
+        ///     分页
+        /// </summary>
+        /// <param name="queryParam"></param>
+        /// <returns></returns>
+        public async Task<PagedResultDto<SystemOperateLogOutput>> GetPagingOperationLog(SystemOperateLogResultRequestDto queryParam)
+        {
+            var list = await _systemOperationLogRepository.GetListAsync(queryParam);
+            var totalCount = await _systemOperationLogRepository.GetCountAsync(queryParam);
+
+            return new PagedResultDto<SystemOperateLogOutput>(
+                totalCount,
+                ObjectMapper.Map<List<SystemOperateLog>, List<SystemOperateLogOutput>>(list)
+                );
+        }
+
+
+    }
+}
